41e95d569ed7e3890903ce5120359a06d9f665cb
Yap8778 <junsong1341@gmail.com>
2025-04-16 01:00:54 +0800

    Consultation

--- Emonie/mean-course/src/app/Consultation/consultation.component.ts
+++ Emonie/mean-course/src/app/Consultation/consultation.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { ConsultationService } from './consultation.service';
 import { Consultation } from './consultation.model';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// Import the material from angular
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
